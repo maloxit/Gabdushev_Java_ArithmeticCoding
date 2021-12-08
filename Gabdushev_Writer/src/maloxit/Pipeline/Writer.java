ae5d0877bcceb3dfd288ec666eec8e008e209ebb
maloxit <61049974+maloxit@users.noreply.github.com>
2021-12-08 15:17:15 +0300

    Some impruvements

--- Gabdushev_Writer/src/maloxit/Pipeline/Writer.java
+++ Gabdushev_Writer/src/maloxit/Pipeline/Writer.java
@@ -8,10 +8,17 @@ import java.nio.IntBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
+class WriterParams {
+
+    public final String BUFFER_SIZE_STR;
+
+    WriterParams(String buffer_size_str) {
+        BUFFER_SIZE_STR = buffer_size_str;
+    }
+}
+
 public class Writer implements IWriter {
 
-
-    private static final String BUFFER_SIZE_CONFIG_NAME = "buffer_size";
     private int BUFFER_SIZE = 0;
     private OutputStream output;
     private BufferedOutputStream bufferedOutput;
@@ -26,38 +33,49 @@ public class Writer implements IWriter {
      */
     @Override
     public RC setConfig(String cfgFileName) {
+        RC rc;
         IUniversalConfigReader config = new UniversalConfigReader();
+        WriterGrammar grammar = new WriterGrammar();
         try {
-            RC rc = config.SetGrammar(new Grammar(
-                    BUFFER_SIZE_CONFIG_NAME
-            ), RC.RCWho.WRITER);
-            if (!rc.isSuccess()) {
+            rc = config.SetGrammar(grammar, RC.RCWho.READER);
+            if (!rc.isSuccess())
                 return rc;
-            }
             rc = config.ParseConfig(new FileReader(cfgFileName));
-            if (!rc.isSuccess()) {
+            if (!rc.isSuccess())
                 return rc;
-            }
         } catch (FileNotFoundException ex) {
-            return RC.RC_WRITER_CONFIG_FILE_ERROR;
+            return RC.RC_READER_CONFIG_FILE_ERROR;
         }
         HashMap<String, String> data = config.GetData();
-        try {
-            String tmp = data.get(BUFFER_SIZE_CONFIG_NAME);
-            BUFFER_SIZE = Integer.parseInt(tmp);
-            if (BUFFER_SIZE <= 0) {
-                return RC.RC_WRITER_CONFIG_SEMANTIC_ERROR;
-            }
-        } catch (NumberFormatException ex) {
-            return RC.RC_WRITER_CONFIG_SEMANTIC_ERROR;
-        }
-        assert (BUFFER_SIZE % Character.BYTES == 0 && BUFFER_SIZE % Integer.BYTES == 0);
-        if (output != null && bufferedOutput == null) {
-            bufferedOutput = new BufferedOutputStream(output, BUFFER_SIZE);
-        }
+        WriterParams params = grammar.WriterParamsFromData(data);
+        if (params == null)
+            return RC.RC_READER_CONFIG_GRAMMAR_ERROR;
+
+        rc = SemanticAnalise(params);
+        if (!rc.isSuccess())
+            return rc;
+
         return RC.RC_SUCCESS;
     }
 
+    /**
+     * Checks if given parameters are semantically correct and initialises readers fields.
+     * @param params Parameters for reader
+     * @return Return Code object, which contains information about reason of the end of work
+     */
+    private RC SemanticAnalise(WriterParams params) {
+        try {
+            String tmp = params.BUFFER_SIZE_STR;
+            BUFFER_SIZE = Integer.parseInt(tmp);
+            if (BUFFER_SIZE <= 0) {
+                return RC.RC_READER_CONFIG_SEMANTIC_ERROR;
+            }
+        } catch (NumberFormatException ex) {
+            return RC.RC_READER_CONFIG_SEMANTIC_ERROR;
+        }
+        assert (BUFFER_SIZE % Character.BYTES == 0 && BUFFER_SIZE % Integer.BYTES == 0);
+        return RC.RC_SUCCESS;
+    }
 
     /**
      * Sets an output stream to write data to
@@ -67,9 +85,6 @@ public class Writer implements IWriter {
     @Override
     public RC setOutputStream(OutputStream output) {
         this.output = output;
-        if (BUFFER_SIZE != 0) {
-            bufferedOutput = new BufferedOutputStream(this.output, BUFFER_SIZE);
-        }
         return RC.RC_SUCCESS;
     }
 
@@ -91,11 +106,12 @@ public class Writer implements IWriter {
 
     /**
      * Processes given data package
-     * @param buff input data package or null if input has reached end
      * @return Return Code object, which contains information about reason of the end of work
      */
     @Override
     public RC consume() {
+        if (bufferedOutput == null)
+            bufferedOutput = new BufferedOutputStream(output, BUFFER_SIZE);
         Object data = mediator.getData();
         if (data == null) {
             try {
